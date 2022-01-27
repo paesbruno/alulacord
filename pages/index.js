f67295a98174390eb504ef091b6c569c6bc3d452
paesbruno <paes.bruno@gmail.com>
2022-01-27 17:09:56 -0300

    Add chat.js

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,8 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 import React from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../config.json';
 
-
-function GlobalStyle() {
-    return (
-      <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-  }
 
 function Title(props) {
     const Tag = props.tag || 'h1';
@@ -64,11 +36,11 @@ function Title(props) {
 export default function PaginaInicial() {
     //const username = 'omariosouto';
     const [username, setUsername] = React.useState('paesbruno');
-    //  const roteamento = useRouter();
+    const roteamento = useRouter();
 
+ 
     return (
         <>
-        <GlobalStyle />
             <Box
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -99,14 +71,14 @@ export default function PaginaInicial() {
                             infosDoEvento.preventDefault();
                             console.log('Alguém submeteu o form');
                             roteamento.push('/chat');
-                            // window.location.href = '/chat';
+                            //window.location.href = '/chat';
                         }}
                         styleSheet={{
                             display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                             width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
                         }}
                     >
-                        <Title tag="h1">Boas vindas de volta!</Title>
+                        <Title tag="h1">Bem vindo(a) de volta!</Title>
                         <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
                             {appConfig.name}
                         </Text>
